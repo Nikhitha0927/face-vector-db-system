8eb42827b011185c01840682342f425bbb4474b8
Nikhitja0927 <nikhitha1027@gmail.com>
2026-05-18 15:38:44 +0530

    Fixed DB initialization and create_tables setup

--- db.py
+++ db.py
@@ -1,6 +1,11 @@
 import os
+
 from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Base
 
 load_dotenv()
 
@@ -10,5 +15,14 @@ engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
 )
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
 
 print("DB connected successfully")
